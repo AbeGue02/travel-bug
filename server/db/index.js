73100cbf7bc3c97d5c565c182b611573fa09892b
AbeGue02 <abrahamgue02@gmail.com>
2024-03-08 16:40:41 -0500

    Initial server commit

---
+++ server/db/index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+mongoose
+    .connect(process.env.MONGODB_CONNECTION_STRING)
+    .then(() => {
+        console.log('Successfully connected to MongoDB.')
+    })
+    .catch(e => {
+        console.error('Connection error', e.message)
+    })
+mongoose.set({debug: true}) //returns console.logs when it does something
+
+const db = mongoose.connection
+
+module.exports = db
