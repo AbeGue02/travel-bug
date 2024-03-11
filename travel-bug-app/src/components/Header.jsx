b9a7598f3a33f96348c3f960e98211d4afdb1037
joy <67973116+aquaj0y@users.noreply.github.com>
2024-03-11 15:39:20 -0500

    header

--- travel-bug-app/src/components/Header.jsx
+++ travel-bug-app/src/components/Header.jsx
@@ -1,7 +1,33 @@
+import { GiButterfly } from "react-icons/gi";
+import { useState, useContext } from 'react'
+import UserContext from '../Context'
+import { useNavigate } from "react-router-dom";
+
+
 export default function Header() {
+    const { user, setUser } = useContext(UserContext)
+
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        if (user.email) {
+            navigate(`/`)
+            console.log('clicked')
+        }
+    }
+
     return (
         <>
-            {/* Should render Logo and Profile Picture */}
+            <div className="logo" onClick={handleClick}>
+                {/* Should render Logo and Profile Picture */}
+                {}
+                <h1><GiButterfly /> Travel Bug</h1>
+            </div>
+            {user.email ? (
+                <div>
+                    <img id="profile-pic" src="https://img.freepik.com/premium-vector/user-profile-icon-flat-style-member-avatar-vector-illustration-isolated-background-human-permission-sign-business-concept_157943-15752.jpg" />
+                </div>
+            ) : <div>no user here</div>}
         </>
     )
 }
