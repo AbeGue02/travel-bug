a7ca569ba4422dd88eeffaf2af129d2dc30344bc
CV2Tre <cvlookin@me.com>
2024-03-13 09:48:51 -0400

    updating trip and tripdetails

--- travel-bug-app/src/components/Trip.jsx
+++ travel-bug-app/src/components/Trip.jsx
@@ -1,8 +1,13 @@
+import React from "react"
+
 export default function Trip({trip}) {
+
     return (
-        <>
+        <div>
             <h4>{trip.fromCity} - {trip.toCity}</h4>
+            <p>Start Date: {trip.startDate}</p>
+            <p>End Date: {trip.endDate}</p>
             <button>Go to Details</button>
-        </>
+        </div>
     )
 }
--- travel-bug-app/src/components/TripDetails.jsx
+++ travel-bug-app/src/components/TripDetails.jsx
@@ -1,7 +1,12 @@
+import React from "react"
+
 export default function TripDetails() {
     return (
-        <>
-            {/* Load up specific details from a trip including all their activities */}
-        </>
+        <div>
+            <h4>{trip.fromCity}, {trip.fromCountry} - {trip.toCity}, {trip.toCountry}</h4>
+            <p>Start Date: {trip.startDate}</p>
+            <p>End Date: {trip.endDate}</p>
+        </div>
+
     )
 }
