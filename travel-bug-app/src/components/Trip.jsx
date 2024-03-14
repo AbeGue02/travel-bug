051cef75b6f5ec801a6cfb8912aa56c632a1b9d5
AbeGue02 <abrahamgue02@gmail.com>
2024-03-13 22:05:16 -0400

    Fixes button inside of Trip.jsx

--- travel-bug-app/src/components/Trip.jsx
+++ travel-bug-app/src/components/Trip.jsx
@@ -1,13 +1,17 @@
 import React from "react"
+import { useNavigate, useParams } from "react-router-dom"
 
 export default function Trip({trip}) {
+
+    const { tripListId } = useParams()
+    let navigate = useNavigate()
 
     return (
         <div>
             <h4>{trip.fromCity} - {trip.toCity}</h4>
             <p>Start Date: {trip.startDate}</p>
             <p>End Date: {trip.endDate}</p>
-            <button>Go to Details</button>
+            <button onClick={() => {navigate(`/${tripListId}/trips/${trip._id}`)}}>Go to Details</button>
         </div>
     )
 }
